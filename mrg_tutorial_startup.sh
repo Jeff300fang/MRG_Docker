4a15c16f0b3d2eb4fd94add57e575f24272e5059
Jeffrey Fang <jeff300fang@gmail.com>
2025-09-02 11:36:56 -0400

    Fix read issue

--- mrg_tutorial_startup.sh
+++ mrg_tutorial_startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get users github username
-read -p "Enter your GitHub username (for cloning your fork): " GH_USERNAME
+read -p "Enter your GitHub username (for cloning your fork): " GH_USERNAME < /dev/tty
 
 # Check if user has forked both repositories
 curl --silent --fail "https://github.com/${GH_USERNAME}/ROS_Tutorial" > /dev/null || { echo "Fork of ROS_Tutorial not found!"; exit 1; }
