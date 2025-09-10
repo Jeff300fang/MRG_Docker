2c5b30a259ef96b2f349292c86180460217d5894
JeffreyFang <jeff300fang@gmail.com>
2025-09-10 10:05:53 -0400

    Update ros_ws_bootstrap.sh

--- ros_ws_bootstrap.sh
+++ ros_ws_bootstrap.sh
@@ -6,5 +6,10 @@ apt-get update -y
 # Install required ROS2 packages
 rosdep install -y --from-paths /root/mrg_ws/src --ignore-src
 
+# Install missing packages
+apt-get install ros-jazzy-robot-localization -y
+apt-get install ros-jazzy-tf-transformations -y
+apt-get install ros-jazzy-urdfdom-py -y
+
 # Source ROS2 setup
 source /opt/ros/jazzy/setup.bash
